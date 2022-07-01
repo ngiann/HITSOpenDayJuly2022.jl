4a398fbb2df2de32c6fb7542cb2c9f1fd7848294
ngiann <ngiann@gmail.com>
2022-07-01 10:29:49 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/HITSOpenDayJuly2022.jl
@@ -0,0 +1,5 @@
+module HITSOpenDayJuly2022
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HITSOpenDayJuly2022
+using Test
+
+@testset "HITSOpenDayJuly2022.jl" begin
+    # Write your tests here.
+end
